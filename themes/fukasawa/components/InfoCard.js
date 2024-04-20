21afe0dfdc759b36d2d2cd664560f4c7f923946f
巡游阳光 <cruisesun@s-Mac.local>
2024-04-20 18:09:07 +0800

    增加个人简介

--- themes/fukasawa/components/InfoCard.js
+++ themes/fukasawa/components/InfoCard.js
@@ -7,7 +7,7 @@ const InfoCard = (props) => {
   return <div id='info-card' className='py-0'>
     <div className='items-center justify-center'>
         <div className='hover:scale-105 transform duration-200 cursor-pointer flex justify-center' onClick={ () => { Router.push('/about') }}>
-            <LazyImage src={siteInfo?.icon} className='rounded-full' width={120} alt={siteConfig('AUTHOR')}/>
+            <LazyImage src={siteInfo?.icon} className='rounded-full' width={100} alt={siteConfig('AUTHOR')}/>
          </div>
         <div className='flex-col flex justify-center'>
             <div className='text-xl py-2 hover:scale-105 transform duration-200 flex justify-center dark:text-gray-300'>{siteConfig('AUTHOR')}</div>

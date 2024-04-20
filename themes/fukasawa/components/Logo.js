110932488e9d9e3c32b4c2c75e50964e1e9e2784
巡游阳光 <cruisesun@s-Mac.local>
2024-04-20 18:39:58 +0800

    增加个人简介

--- themes/fukasawa/components/Logo.js
+++ themes/fukasawa/components/Logo.js
@@ -6,7 +6,7 @@ const Logo = props => {
    <section className='flex'>
     <Link
      href='/'
-     className='hover:bg-black hover:text-white border-black border-2 duration-500 px-6 py-3 cursor-pointer dark:text-gray-300 dark:border-gray-300 font-black text-xl'>
+     className='hover:bg-black hover:text-white border-black border-2 duration-500 px-4 py-2 cursor-pointer dark:text-gray-300 dark:border-gray-300 font-black text-lg'>
      {siteConfig('TITLE')}
     </Link>
    </section>

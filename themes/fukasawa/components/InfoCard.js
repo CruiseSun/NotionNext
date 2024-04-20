cb5729fba85a8e349f34be1e78a0e4580467946a
巡游阳光 <cruisesun@s-Mac.local>
2024-04-20 23:00:38 +0800

    增加个人简介

--- themes/fukasawa/components/InfoCard.js
+++ themes/fukasawa/components/InfoCard.js
@@ -5,13 +5,13 @@ import { siteConfig } from '@/lib/config'
 const InfoCard = (props) => {
   const { siteInfo } = props
   return <div id='info-card' className='py-0'>
-    <ul class="flex justify-center space-x-5 pb-4 dark:text-gray-400 text-gray-600 overflow-auto"><li class="font-black border-b-2 border-red-600 text-red-600 animate__animated animate__jello text-sm font-sans">About</li></ul>
+    <ul class="flex justify-center space-x-5 pb-4 dark:text-gray-400 text-gray-600 overflow-auto"><li class="font-black border-b-2 border-red-600 text-red-600 animate__animated animate__jello text-lg font-sans">About</li></ul>
     <div className='items-center justify-center'>
         <div className='hover:scale-105 transform duration-200 cursor-pointer flex justify-center pt-4' onClick={ () => { Router.push('/about') }}>
             <LazyImage src={siteInfo?.icon} className='rounded-full' width={100} alt={siteConfig('AUTHOR')}/>
          </div>
         <div className='flex-col flex justify-center'>
-            <div className='text-lg py-2 duration-200 flex justify-center dark:text-gray-300'>{siteConfig('AUTHOR')}</div>
+            <div className='text-base py-2 duration-200 flex justify-center dark:text-gray-300 font-sans'>{siteConfig('AUTHOR')}</div>
             <div className='font-light text-gray-600 mb-1 duration-200 flex justify-center dark:text-gray-400'>{siteConfig('BIO')}</div>
         </div>
     </div>
